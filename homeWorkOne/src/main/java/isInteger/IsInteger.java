772589c48830142e06e60b7b877e2aa25d14cf93
Hovhannes Hovhannisyan <hovhannes.hovhannisyan.576@my.csun.edu>
2025-09-14 21:13:40 -0700

    Update IsInteger.java

--- homeWorkOne/src/main/java/isInteger/IsInteger.java
+++ homeWorkOne/src/main/java/isInteger/IsInteger.java
@@ -1,4 +1,5 @@
 package isInteger;
+import java.util.Scanner;
 
 public class IsInteger {
 
@@ -9,5 +10,18 @@ public class IsInteger {
 //
 //    Return true if it is an integer, or false if it is not.
 //
-
+  public static boolean isInteger(String text){
+    for(int i = 0; i < text.length(); i++){
+      int character = text.charAt(i) - '0'; // Converts char to int
+      if(character < 0 || character > 9) // Checks if the number is a decimal digit from 0-9, otherwise is not an int character
+        return false;
+    }
+    return true;
+  }
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a string to determine if its an int or not.");
+    String s = input.nextLine();
+    System.out.println(isInteger(s));
+  }
 }
