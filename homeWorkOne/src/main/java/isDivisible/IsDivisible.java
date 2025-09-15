c020293645ee66f0579852539aece754b435a2f2
Hovhannes Hovhannisyan <hovhannes.hovhannisyan.576@my.csun.edu>
2025-09-14 21:35:34 -0700

    Update IsDivisible.java

--- homeWorkOne/src/main/java/isDivisible/IsDivisible.java
+++ homeWorkOne/src/main/java/isDivisible/IsDivisible.java
@@ -1,4 +1,5 @@
 package isDivisible;
+import java.util.Scanner;
 
 //Write a method that returns whether a is divisible by b.
 //
@@ -8,12 +9,20 @@ package isDivisible;
 
 public class IsDivisible {
 
-  public boolean isDivisible(int a, int b){
+  public static boolean isDivisible(int a, int b){
     if(b == 0)
       return false;
     else if(a % b == 0)
       return true;
     else
       return false;
+  }
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a dividend: ");
+    int dividend = input.nextInt();
+    System.out.print("Enter a divisor: ");
+    int divisor = input.nextInt();
+    System.out.print(isDivisible(dividend, divisor));
   }
 }
