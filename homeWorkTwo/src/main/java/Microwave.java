98484c37eb3031ff8cf73643064ce94b788ceb9a
Hovhannes Hovhannisyan <hovhannes.hovhannisyan.576@my.csun.edu>
2025-09-29 01:09:22 -0700

    Update Microwave.java

--- homeWorkTwo/src/main/java/Microwave.java
+++ homeWorkTwo/src/main/java/Microwave.java
@@ -1,2 +1,7 @@
 public class Microwave extends Device {
+  public int maxTime;
+  Microwave(int startPower, int maxCookingTime) {
+    super(Device(startPower, maxCookingTime);
+    maxTime = maxCookingTime;
+  }
 }
