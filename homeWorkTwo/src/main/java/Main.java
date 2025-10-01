1f2c246f20bce3616561f5b29813cbd7b4161062
Hovhannes Hovhannisyan <hovhannes.hovhannisyan.576@my.csun.edu>
2025-10-01 08:15:21 -0700

    Update Main.java

--- homeWorkTwo/src/main/java/Main.java
+++ homeWorkTwo/src/main/java/Main.java
@@ -47,7 +47,7 @@ public class Main {
         Microwave m1 = new Microwave(2, 1);
         Device polyDevice;
         polyDevice = m1;
-        polyDevice.printDescription(); // polyDevice is printed as a device due to the runtime polyDevice being treated as a Device object
+        polyDevice.printDescription(); // polyDevice is printed as a Microwave due to the runtime polyDevice being treated as a Microwave object
 
 
     }
