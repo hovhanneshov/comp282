7c1185cb9e9ce3bdc30c3e5db44321291ad32f73
Hovhannes Hovhannisyan <hovhannes.hovhannisyan.576@my.csun.edu>
2025-12-10 05:17:24 -0800

    Update RecursionExercises.java
    feat: added a main method that prints all recursionexercise tasks 1-5

--- homeWorkFive/recursionexample/RecursionExercises.java
+++ homeWorkFive/recursionexample/RecursionExercises.java
@@ -70,4 +70,36 @@ public class RecursionExercises {
             return s;
         return s.substring(s.length()-1) + reverse(s.substring(0, s.length()-1));
     }
+
+    // Added main method to test the tasks
+    public static void main(String[] args) {
+        // Task 1
+        System.out.print("\nPrinting factorial of 0: " + factorialRecursive(0));
+        //System.out.print("\nPrinting factorial of -1: " + factorialRecursive(-1));
+        System.out.print("\nPrinting factorial of 5: " + factorialRecursive(5));
+
+        // Task 2
+        System.out.print("\nPrinting factorial of 0: " + factorialIterative(0));
+        //System.out.print("\nPrinting factorial of -1: " + factorialIterative(-1));
+        System.out.print("\nPrinting factorial of 5: " + factorialIterative(5));
+
+        // Task 3
+        System.out.print("\nPrinting Fibonacci of 0: " + fibonacciRecursive(0));
+        //System.out.print("\nPrinting Fibonacci of -1: " + fibonacciRecursive(-1));
+        System.out.print("\nPrinting Fibonacci of 2: " + fibonacciRecursive(2));
+
+        // Task 4
+        int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr2 = new int[0];
+        int[] arr3 = null;
+        System.out.print("\nPrinting the sum of arr1: " + sumArray(arr1));
+        System.out.print("\nPrinting the sum of arr2: " + sumArray(arr2));
+        //System.out.println("\nPrinting the sum of arr3: " + sumArray(arr3));
+
+        // Task 5
+        String s1 = "cat";
+        String s2 = null;
+        System.out.print("\nPrinting " + s1 + " backwards: " +reverse(s1));
+        //System.out.print("\nPrinting " + s2 + " backwards: " +reverse(s2));
+    }
 }
